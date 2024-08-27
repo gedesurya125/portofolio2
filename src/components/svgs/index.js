b33c097fbd3bc2276dc4344a361e202a25126a48
gedesurya125 <gedesurya125@gmail.com>
2024-08-28 01:03:06 +0800

    Add Main logo asset

---
+++ src/components/svgs/index.js
@@ -0,0 +1,1 @@
+export * from './SuryaLogo';
--- src/sections/landingPage/header/Header.js
+++ src/sections/landingPage/header/Header.js
@@ -1,14 +1,16 @@
+import { SuryaLogo } from 'components/svgs';
 import { ResponsiveGrid } from 'components/theme';
 import React from 'react';
 
 export const Header = () => {
   return (
     <ResponsiveGrid as="header">
+      <SuryaLogo className="h-auto w-10" />
       <Headline />
     </ResponsiveGrid>
   );
 };
 
 const Headline = () => {
-  return <h1 className="font-heading text-lg font-bold italic">Surya A</h1>;
+  return <h1 className="text-9xl whitespace-nowrap font-heading font-bold italic">Surya A</h1>;
 };
