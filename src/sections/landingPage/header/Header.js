2fd59c84f4f87403cdabde2bfc91b9f37f61cc13
gedesurya125 <gedesurya125@gmail.com>
2024-09-11 00:59:41 +0800

    add header component

--- src/sections/landingPage/header/Header.js
+++ src/sections/landingPage/header/Header.js
@@ -1,20 +1,34 @@
+import clsx from 'clsx';
 import { Logo, SuryaLogo } from 'components/svgs';
 import { ResponsiveGrid } from 'components/theme';
 import React from 'react';
 
 export const Header = () => {
   return (
-    <ResponsiveGrid as="header">
-      <HeaderLogo />
-      <Headline />
-    </ResponsiveGrid>
+    <div className={clsx('lg:flex lg:h-screen lg:items-center')}>
+      <ResponsiveGrid as="header" className={clsx('')}>
+        <HeaderLogo />
+        <Headline />
+      </ResponsiveGrid>
+    </div>
   );
 };
 
 const HeaderLogo = () => {
-  return <Logo className="h-auto w-full" />;
+  return <Logo className={clsx('md:w-10', 'col-end-8 lg:col-start-2 lg:w-full')} />;
 };
 
 const Headline = () => {
-  return <h1 className="font-heading text-9xl">I make websites</h1>;
+  return (
+    <div className={clsx('lg:col-start-12 lg:col-end-24 lg:self-center')}>
+      <span className={clsx('font-body', 'lg:text-[3.1rem]')}>Hello my name is Surya.</span>
+      <h1 className={clsx('font-heading', 'lg:text-[7rem]')}>
+        I make <span className={clsx('text-secondary')}>websites</span>
+      </h1>
+      <p className={clsx('font-body', 'lg:text-[3rem]')}>
+        I’m a front-end developer
+        <br /> and website designer
+      </p>
+    </div>
+  );
 };
