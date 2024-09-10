ab779ec85bd4ebc78d3ae201e85f34e6df815269
gedesurya125 <gedesurya125@gmail.com>
2024-09-11 00:17:56 +0800

    clean up the header section

--- src/app/page.js
+++ src/app/page.js
@@ -5,28 +5,6 @@ export default async function Home() {
   return (
     <main>
       <Header />
-      <OutsideGridComponent />
-      <InsideGridComponent />
     </main>
   );
 }
-
-const OutsideGridComponent = () => {
-  return (
-    <div className="__outside-grid-component mt-20">
-      <div className="box h-card-lg w-card-lg bg-slate-200" />
-      <p className="mt-2 font-body">This is outside grid component</p>
-    </div>
-  );
-};
-
-const InsideGridComponent = () => {
-  return (
-    <ResponsiveGrid className="mt-20">
-      <div className="col-start-1 col-end-6 h-[3rem] bg-white" />
-      <p className="col-start-1 col-end-12 mt-2 font-body">
-        This is inside grid component with col-start-1 and col-end-6
-      </p>
-    </ResponsiveGrid>
-  );
-};
--- src/sections/landingPage/header/Header.js
+++ src/sections/landingPage/header/Header.js
@@ -1,15 +1,20 @@
-import { SuryaLogo } from 'components/svgs';
+import { Logo, SuryaLogo } from 'components/svgs';
 import { ResponsiveGrid } from 'components/theme';
 import React from 'react';
 
 export const Header = () => {
   return (
     <ResponsiveGrid as="header">
+      <HeaderLogo />
       <Headline />
     </ResponsiveGrid>
   );
 };
 
+const HeaderLogo = () => {
+  return <Logo className="h-auto w-full" />;
+};
+
 const Headline = () => {
-  return <h1 className="whitespace-nowrap font-heading text-9xl">I make websites</h1>;
+  return <h1 className="font-heading text-9xl">I make websites</h1>;
 };
